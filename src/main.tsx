7e52fba74c0cf13a3755f794c7cb8f1a9e3bd3fb
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-05-31 01:15:24 -0300

    install react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+
+// import react-router-dom
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/index'
 
+// import react-query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient} >
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
