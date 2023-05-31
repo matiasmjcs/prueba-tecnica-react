5323d69467fa24829389498632578e05b348e98c
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-05-31 01:01:19 -0300

    install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  return <>Prueba Tecnica React</>
+  return (
+    <>
+      <Outlet />
+    </>
+  )
 }
 
 export default App
